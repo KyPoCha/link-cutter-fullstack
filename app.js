91f4d895f8c90826e86167ef082370cdde5b0558
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-09-20 12:20:30 +0200

    Update for deploying v2.1.5

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ if(process.env.NODE_ENV === "production"){
     app.use("/",express.static(path.join(__dirname, "client", "build")));
 
     app.get("*",(req, res)=>{
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        const index = path.join(__dirname, 'client' ,'build', 'index.html');
+        res.sendFile(index);
     });
 }
 
